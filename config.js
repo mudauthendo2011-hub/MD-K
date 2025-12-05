39deafcf1ddfef9fea8e75a421831a298fa57137
mudauthendo2011-hub <mudauthendo2011@gmail.com>
2025-12-05 09:57:35 +0200

    Update config.js

--- config.js
+++ config.js
@@ -4,9 +4,9 @@ if (existsSync('config.env')) require('dotenv').config({ path: './config.env' })
 
 module.exports = { 
     SESSION_ID: process.env.SESSION_ID || '',
-    OWNER_NUMBER: process.env.OWNER_NUMBER || "2349067339193",
-    WORKTYPE: process.env.WORKTYPE || "private",
-    PREFIX: process.env.PREFIX || "[.]",
+    OWNER_NUMBER: process.env.OWNER_NUMBER || "27799648540",
+    WORKTYPE: process.env.WORKTYPE || "public",
+    PREFIX: process.env.PREFIX || "[./]",
     ALWAYS_ONLINE: process.env.ALWAYS_ONLINE || true,
     MONGODB_URI: process.env.MONGODB_URI || "",
     STATUS_VIEW: process.env.STATUS_VIEW || true,
@@ -16,23 +16,23 @@ module.exports = {
     ERROR_MSG: process.env.ERROR_MSG || true,
     TIMEZONE: process.env.TIMEZONE || "Africa/Lagos",
     CAPTION: process.env.CAPTION || "",
-    STICKER_PACKNAME: process.env.STICKER_PACKNAME || "Kord",
-    STICKER_AUTHOR: process.env.STICKER_AUTHOR || "🤍",
+    STICKER_PACKNAME: process.env.STICKER_PACKNAME || "ALPHA HUNTER'S PACK",
+    STICKER_AUTHOR: process.env.STICKER_AUTHOR || "thendo",
     BOT_PRESENCE: process.env.BOT_PRESENCE || "available",
     REACT: process.env.REACT || false,
     READ_MESSAGE: process.env.READ_MESSAGE || "false",
-    OWNER_NAME: process.env.OWNER_NAME || "Mirage",
-    BOT_NAME: process.env.BOT_NAME || "Kord",
+    OWNER_NAME: process.env.OWNER_NAME || "Mudau Thendo",
+    BOT_NAME: process.env.BOT_NAME || "",
     RENDER_API_KEY: process.env.RENDER_API_KEY,
     ANTIDELETE: process.env.ANTIDELETE || "on",
     ANTIDELETE_INCHAT: process.env.ANTIDELETE_INCHAT || "off",
     ANTI_EDIT: process.env.ANTI_EDIT || "off",
     ANTI_EDIT_IN_CHAT: process.env.ANTI_EDIT_IN_CHAT || "off",
-    AUDIO_DATA: process.env.AUDIO_DATA || "Kord;🤍",
+    AUDIO_DATA: process.env.AUDIO_DATA || "ALPHA SOUND 🔊;😈",
     SUDO: process.env.SUDO || "",
     MODS: process.env.MODS || "",
-    REJECT_CALL: process.env.REJECT_CALL || "off",
-    WARNCOUNT: process.env.WARNCOUNT || "4",
+    REJECT_CALL: process.env.REJECT_CALL || "on",
+    WARNCOUNT: process.env.WARNCOUNT || "5",
     LANG_CODE: process.env.LANG_CODE || "en",
     VV_CMD: process.env.VV_CMD || "👀",
     SAVE_CMD: process.env.SAVE_CMD || "📥",
@@ -41,11 +41,27 @@ module.exports = {
     LOG_MESSAGES: process.env.LOG_MESSAGES || "off",
     STARTUP_MSG: process.env.STARTUP_MSG || "on",
     MENU_IMAGE: process.env.MENU_IMAGE || "",
-    WELCOME_MESSAGE: process.env.WELCOME_MESSAGE ||  `╭━━━々 𝚆 𝙴 𝙻 𝙲 𝙾 𝙼 𝙴 々━━━╮
-┃ ➺ *々 Welcome @user! to @gname*
-┃ ➺ *々 Members: @count*
-┃ ➺ We Hope You Have A Nice Time Here!
-┗━━━━━━━━━━━━━━━━━━━━━━━━━━━`,
+    WELCOME_MESSAGE: process.env.WELCOME_MESSAGE ||  `@pp ∘₊✧──────✧₊∘
+
+      *@gname*
+
+∘₊✧──────✧₊∘
+
+*USER:* @user 
+*TIME* @time
+*MEMBER:* @count
+
+✧────⋆⋅☆⋅⋆────✧
+
+ *BOT OWNER:* Mudau thendo
++27799648540 
+*GROUP OWNER:*not-set
+
+✧────⋆⋅☆⋅⋆────✧
+
+     @gdesc
+
+∘₊✧──────✧₊∘`,
     GOODBYE_MESSAGE: process.env.GOODBYE_MESSAGE || `╭━━━々 𝙶 𝙾 𝙾 𝙳 𝙱 𝚈 𝙴 々━━━╮
 ┃ ➺ *々 @user! left @gname!*
 ┃ ➺ *々 Members: @count*
